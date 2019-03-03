e82463b9cf6b10685094bf910074e4fb474dc1e4
kaizige10 <m13675637818@163.com>
2019-03-03 21:17:16 +0800

    修复登录失败问题

--- db/users.js
+++ db/users.js
@@ -1,13 +1,13 @@
 exports.users = [{
     id: 0,
     name: 'tom',
-    password: 123456
+    password: "123456"
 }, {
     id: 1,
     name: 'steve',
-    password: 123456
+    password: "123456"
 }, {
     id: 2,
     name: 'jack',
-    password: 123456
+    password: "123456"
 }]
